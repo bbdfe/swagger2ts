a7ab6a03a3b023955939cd50b23ded21a08efe44
xiexingen <1002275364@qq.com>
2022-05-01 11:42:52 +0800

    feat: 模板修改

--- src/example.ts
+++ src/example.ts
@@ -3,9 +3,10 @@ import { generateService } from '.';
 generateService({
   requestImportStatement:`import { API_PREFIX } from '@/constants';import { request } from "@/utils/request"`,
   // schemaPath: 'https://petstore.swagger.io/v2/swagger.json',
-  schemaPath: '../test/spider-swagger.json',
+  // schemaPath: '../assets/spider-swagger.json',
+  schemaPath:'https://localhost:44399/swagger/v1/swagger.json',
   projectName: '',
   serversPath: './servers',
-  requestOptionType: 'BBDFE.TRequestOption',
+  requestOptionType: 'BBDFE.IRequestOption',
   apiPrefix: 'API_PREFIX',
 });
